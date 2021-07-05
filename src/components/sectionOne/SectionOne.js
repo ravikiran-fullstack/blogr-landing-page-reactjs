222cd4f27caa188c2065dc4fa5b9bee0a1174fe7
ravikiran <ravikiransjce.jobs@gmail.com>
2021-07-05 11:50:50 +0530

    section2 desktop and mobile version almost done, need to fine tune a bit

--- src/components/sectionOne/SectionOne.js
+++ src/components/sectionOne/SectionOne.js
@@ -1,7 +1,48 @@
 import React from "react";
+import "./sectionOne-styles.css";
 
 const SectionOne = () => {
-  return <div></div>;
+  return (
+    <section className="sectionOne">
+      <div className="sectionOneHeading">
+        <h1>Designed for the future</h1>
+      </div>
+      <div className="sectionOneBody">
+        <div className="sectionOneInfo">
+          <div className="sectionOneInfoPara">
+            <div className="sectionOneInfoParaHeading">
+              <h2>Introducing an extensible editor</h2>
+            </div>
+            <div className="sectionOneInfoParaContent">
+              <p>
+                Blogr features an exceedingly intuitive interface which lets you
+                focus on one thing: creating content. The editor supports
+                management of multiple blogs and allows easy manipulation of
+                embeds such as images, videos, and Markdown. Extensibility with
+                plugins and themes provide easy ways to functionality or change
+                the looks of a blog
+              </p>
+            </div>
+          </div>
+          <div className="sectionOneInfoPara">
+            <div className="sectionOneInfoParaHeading">
+              <h2>Introducing an extensible editor</h2>
+            </div>
+            <div className="sectionOneInfoParaContent">
+              <p>
+                Blogr features an exceedingly intuitive interface which lets you
+                focus on one thing: creating content. The editor supports
+                management of multiple blogs and allows easy manipulation of
+                embeds such as images, videos, and Markdown. Extensibility with
+                plugins and themes provide easy ways to functionality or change
+                the looks of a blog
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SectionOne;
