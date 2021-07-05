7fc0e60e05012d20a9da04b8c7759c0fdbbaa893
ravikiran <ravikiransjce.jobs@gmail.com>
2021-07-05 14:52:30 +0530

    working on cascading bg images in section two

--- src/components/sectionTwo/SectionTwo.js
+++ src/components/sectionTwo/SectionTwo.js
@@ -1,7 +1,33 @@
 import React from "react";
+import "./sectionTwo-style.css";
+import phoneImage from "../../assets/illustration-phones.svg";
+import circle from "../../assets/bg-pattern-circles.svg";
 
 const SectionTwo = () => {
-  return <div></div>;
+  return (
+    <section className="sectionTwo">
+      <div className="illustrationPhones">
+        <div className="circleBg">
+          <img src={circle} alt="circle" />
+        </div>
+        <img src={phoneImage} alt="phoneImage" />
+      </div>
+      <div className="sectionTwoInfoPara">
+        <div className="sectionTwoInfoParaHeading">
+          <h2>Introducing an extensible editor</h2>
+        </div>
+        <div className="sectionTwoInfoParaContent">
+          <p>
+            Blogr features an exceedingly intuitive interface which lets you
+            focus on one thing: creating content. The editor supports management
+            of multiple blogs and allows easy manipulation of embeds such as
+            images, videos, and Markdown. Extensibility with plugins and themes
+            provide easy ways to functionality or change the looks of a blog
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SectionTwo;
